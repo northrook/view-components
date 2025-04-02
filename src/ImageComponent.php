a0d197061ca8b0692ac19c14aae241b3b051b0eb
Martin <hello@northrook.com>
2025-04-02 09:04:56 +0100

    mend

--- src/ImageComponent.php
+++ src/ImageComponent.php
@@ -7,11 +7,12 @@ namespace Core\View\Component;
 use Core\Asset\ImageAsset;
 use Core\AssetManager;
 use Core\View\Attribute\ViewComponent;
-use Core\View\Element;
-use Core\View\Template\AbstractComponent;
+use Core\View\Template\{Compiler\Nodes\Html\ElementNode, Compiler\Position, Component};
+use Core\View\Template\Compiler\Node;
+use RuntimeException;
 
 #[ViewComponent( ['img', 'img:{type}'], true, 60 )]
-final class ImageComponent extends AbstractComponent
+final class ImageComponent extends Component
 {
     protected string $source;
 
@@ -21,25 +22,58 @@ final class ImageComponent extends AbstractComponent
 
     public function __construct( private readonly AssetManager $assetManager ) {}
 
-    protected function prepareArguments( array &$arguments ) : void
+    public function render() : string
     {
-        if ( \is_string( $arguments['attributes']['src'] ?? null ) ) {
-            $this->source = $arguments['attributes']['src'];
-            unset( $arguments['attributes']['src'] );
+        return __METHOD__;
+    }
+
+    public function getElementNode( ?Position $position = null, ?ElementNode $parent = null ) : ElementNode
+    {
+        $node = $this->getNode();
+
+        if ( ! $node instanceof ElementNode ) {
+            dd( \get_defined_vars(), $this );
+            throw new RuntimeException();
         }
+
+        $node->parent   = $parent;
+        $node->position = $position;
+
+        return $node;
     }
 
-    public function getView() : Element
+    protected function getNode() : Node
     {
-        $this->resolveAsset();
+        $engine = $this->getEngine();
 
-        $this->view->attributes(
-            src : '#',
-            alt : 'Image Component',
-        );
+        // $template = $engine->createTemplate(
+        //     'component/image.latte',
+        //     ['src' => $this->source],
+        // );
 
-        return $this->view;
+        $template = 'component/image.latte';
+        // $template = $this->template();
+
+        $ast = $engine->parse( $template );
+
+        $engine->applyPasses( $ast );
+
+        $string = $engine->generate( $ast, $template );
+        dump( \get_defined_vars() );
+        return $ast->main;
     }
+
+    // private function template() : string
+    // {
+    //     return /** @lang Latte */ <<<'LATTE'
+    //         <figure>
+    //             <img src="{$src ?? '#'}">
+    //             <figcaption>
+    //                 Caption Text
+    //             </figcaption>
+    //         </figure>
+    //         LATTE;
+    // }
 
     protected function resolveAsset() : void
     {
@@ -52,5 +86,13 @@ final class ImageComponent extends AbstractComponent
         \assert( $imageAsset instanceof ImageAsset );
 
         $this->asset = $imageAsset;
+    }
+
+    protected function prepareArguments( array &$arguments ) : void
+    {
+        if ( \is_string( $arguments['attributes']['src'] ?? null ) ) {
+            $this->source = $arguments['attributes']['src'];
+            unset( $arguments['attributes']['src'] );
+        }
     }
 }
