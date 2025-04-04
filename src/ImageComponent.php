ecf5214fed3b47cc7710dd71dfbee30214016763
Martin <hello@northrook.com>
2025-04-04 07:02:08 +0100

    mend

--- src/ImageComponent.php
+++ src/ImageComponent.php
@@ -4,17 +4,17 @@ declare(strict_types=1);
 
 namespace Core\View\Component;
 
-use Core\Asset\ImageAsset;
 use Core\AssetManager;
+use Core\Asset\ImageAsset;
 use Core\View\Attribute\ViewComponent;
 use Core\View\Template\Component;
-use Core\View\Template\Runtime\Html;
-use Core\View\Template\Runtime\HtmlStringable;
 
 #[ViewComponent( ['img', 'img:{type}'], true, 60 )]
 final class ImageComponent extends Component
 {
-    public ?string $source = null;
+    protected const string FALLBACK = '';
+
+    public string $source;
 
     public ?string $type = null;
 
@@ -22,51 +22,26 @@ final class ImageComponent extends Component
 
     public function __construct( private readonly AssetManager $assetManager ) {}
 
-    protected function resolveAsset() : void
-    {
-        if ( isset( $this->image ) ) {
-            return;
-        }
-
-        $imageAsset = $this->assetManager->getAsset( $this->source );
-
-        \assert( $imageAsset instanceof ImageAsset );
-
-        $this->image = $imageAsset;
-    }
-
     protected function prepareArguments( array &$arguments ) : void
     {
         if ( \is_string( $arguments['attributes']['src'] ?? null ) ) {
             $this->source = $arguments['attributes']['src'];
             unset( $arguments['attributes']['src'] );
         }
+        else {
+            $this->source = $this::FALLBACK;
+        }
     }
 
     protected function getTemplateParameters() : self
     {
+        if ( ! isset( $this->image ) ) {
+            $imageAsset = $this->assetManager->getAsset( $this->source );
+
+            \assert( $imageAsset instanceof ImageAsset );
+
+            $this->image = $imageAsset;
+        }
         return $this;
     }
-
-    // private function getImage() : HtmlStringable
-    // {
-    //     $imageAsset = $this->assetManager->getAsset( $this->source );
-    //     // var_dump( $this->image );
-    //     return new Html(
-    //             $imageAsset->getPicture(
-    //             // component_id : $this->uniqueID,
-    //             ),
-    //     );
-    // }
-    //
-    // /**
-    //  * @return array{src: string}
-    //  */
-    // protected function getTemplateParameters() : array
-    // {
-    //     return [
-    //             'image'  => $this->getImage(),
-    //             'source' => $this->source ?? 'lamb source',
-    //     ];
-    // }
 }
