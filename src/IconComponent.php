1180e05c53d09d5e9dba9d8d278e21eb1218da01
Martin <hello@northrook.com>
2025-04-27 19:36:36 +0100

    mend

--- src/IconComponent.php
+++ src/IconComponent.php
@@ -8,7 +8,7 @@ use Core\View\ComponentFactory\ViewComponent;
 use InvalidArgumentException;
 use RuntimeException;
 
-#[ViewComponent( 'icon:{get}:{size}' )]
+#[ViewComponent( 'icon:{name}:{mod}' )]
 final class IconComponent extends Component
 {
     public Element $svg;
@@ -16,23 +16,37 @@ final class IconComponent extends Component
     public function __construct( private readonly IconProviderService $iconProvider ) {}
 
     /**
-     * @param string $get
-     * @param ?int   $size
-     * @param mixed  ...$attributes
+     * @param string  $name
+     * @param ?string $mod
+     * @param mixed   ...$attributes
      *
      * @return $this
      */
     public function __invoke(
-        ?string  $get = null,
-        ?int     $size = null,
+        ?string  $name = null,
+        ?string  $mod = null,
         mixed ...$attributes,
     ) : self {
-        if ( ! $get ) {
-            $this->logger?->error( $this::class.': No icon key provided.' );
-            throw new InvalidArgumentException( 'No icon key provided.' );
+        if ( ! $name ) {
+            $message = $this::class.': No icon name provided.';
+            $this->logger?->error( $message );
+            throw new InvalidArgumentException( $message );
         }
-        $this->svg = $this->iconProvider->getSvg( $get, ...$attributes )
-                     ?? throw new RuntimeException( 'No icon found.' );
+
+        if ( $mod ) {
+            $name .= ":{$mod}";
+        }
+
+        if ( $this->attributes->merge( ...$attributes )->has( 'size' ) ) {
+            $size                 = $this->attributes->pull( 'size' );
+            $size                 = \is_int( $size ) ? "{$size}px" : $size;
+            $attributes['width']  = $size;
+            $attributes['height'] = $size;
+        }
+        $this->svg = $this->iconProvider->getSvg( $name, ...$attributes )
+                     ?? throw new RuntimeException(
+                         $this::class."No icon found for '{$name}'.",
+                     );
         return $this;
     }
 }
--- src/SvgComponent.php
+++ src/SvgComponent.php
@@ -8,7 +8,7 @@ use Core\View\ComponentFactory\ViewComponent;
 use InvalidArgumentException;
 use RuntimeException;
 
-#[ViewComponent( 'svg:{get}' )]
+#[ViewComponent( 'svg:{name}:{mod}' )]
 final class SvgComponent extends Component
 {
     public readonly Element $svg;
@@ -16,23 +16,31 @@ final class SvgComponent extends Component
     public function __construct( private readonly IconProviderService $iconProvider ) {}
 
     /**
-     * @param string $get
-     * @param mixed  ...$attributes
+     * @param string          $name
+     * @param null|int|string $size
+     * @param mixed           ...$attributes
      *
      * @return $this
      */
     public function __invoke(
-        ?string  $get = null,
-        mixed ...$attributes,
+        ?string         $name = null,
+        null|int|string $size = null,
+        mixed        ...$attributes,
     ) : self {
-        if ( ! $get ) {
-            $this->logger?->error( $this::class.': No icon key provided.' );
-            throw new InvalidArgumentException( 'No icon key provided.' );
+        if ( ! $name ) {
+            $message = $this::class.': No icon name provided.';
+            $this->logger?->error( $message );
+            throw new InvalidArgumentException( $message );
         }
-        $this->svg = $this->iconProvider->getSvg( $get, ...$attributes )
-                     ?? throw new RuntimeException( 'No icon found.' );
-
-        dump( [__METHOD__ => $this, ...\get_defined_vars()] );
+        if ( $size !== null ) {
+            $size                 = \is_int( $size ) ? "{$size}px" : $size;
+            $attributes['width']  = $size;
+            $attributes['height'] = $size;
+        }
+        $this->svg = $this->iconProvider->getSvg( $name, ...$attributes )
+                     ?? throw new RuntimeException(
+                         $this::class."No icon found for '{$name}'.",
+                     );
         return $this;
     }
 
